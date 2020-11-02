7fb5a6e690cfdec38ee4501e7d7710fa5075d27c
Carly Bouts <cbouts@g.clemson.edu>
2020-11-02 14:12:09 -0500

    update deep link file

--- cmcap_deeplink.py
+++ cmcap_deeplink.py
@@ -16,13 +16,18 @@ print(df['link'])
 # testing this is around 8 minutes in 10/06
 
 for link in df['link']:
-# figure out how to do this only for the first 500 links
-	print('link')
 	filename = link.replace('/currencies/', '')
-	response = urllib.request.urlopen("https://coinmarketcap.com/" + link)
-	html = response.read()
-	open('deep_link_html/' + filename  + '.html', 'wb')
-	f.write(html)
-	f.close()
-	time.sleep(30)
+	if os.path.exists("deep_link_html/" + filename + ".html")
+		print(filename + ".html already exists.")
+		# if statement makes it robust against interruptions, also ensures that we don't re-download the same files once it has looped through the first 500 links.
+	else
+		print("downloading " + 'link')
+		response = urllib.request.urlopen("https://coinmarketcap.com/" + link)
+		html = response.read()
+		open('deep_link_html/' + filename + '.html.temp', 'wb')
+		# .temp is added to prevent writing empty files when we have interruptions (around 32:50 of 10/06). The file is only renamed into the final format after everything is already written properly. temp prevents it from stopping when it messes up.
+		f.write(html)
+		f.close()
+		os.rename("deep_link_html/" + filename + '.html.temp', 'deep_link_html/' + filename + '.html')
+		time.sleep(30)
 
