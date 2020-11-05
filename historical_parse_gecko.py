4027f90961937b2400df42316df711304af8a4fe
Carly Bouts <cbouts@g.clemson.edu>
2020-11-04 21:01:32 -0500

    write gecko historical parse, modify gecko parse

--- historical_parse_gecko.py
+++ historical_parse_gecko.py
@@ -2,44 +2,33 @@ import json
 import pandas as pd 
 import glob
 import os
-import pdb
+from datetime import datetime
+import tzlocal
 
 if not os.path.exists('coingecko_parsed_files'):
 	os.mkdir('coingecko_parsed_files')
 
 df = pd.DataFrame()
-# will need to change the source folder in the line below to historical_json_files
+
 for json_file_name in glob.glob('historical_json_files/*.json'):
-# json_file_name = 'json_files_trial/coingecko20201101164950.json'
-	print("parsing" + json_file_name)
-	scrape_time = os.path.basename(json_file_name).replace("coingecko","").replace(".json","")
+# json_file_name = 'historical_json_files/0x.json'
+	print("parsing " + json_file_name)
 	f = open(json_file_name, "r")
 	json_data = json.load(f)
+	for i in range(1,365):
+		unix_timestamp = json_data['prices'][i][0]
+		local_timezone = tzlocal.get_localzone()
+		local_time = datetime.fromtimestamp(unix_timestamp/1000, local_timezone)
+		date = local_time.strftime("%Y%m%d")
+		print(date)
+		
+		df = df.append({
+				'date': datetime.datetime.utcfromtimestamp(unix_timestamp).strftime("%Y%m%d%H%M%S")
+				# CONVERT THIS!
+				'price': json_data['prices'][i][1]
+				'market_caps': json_data['market_caps'][i][1]
+				'total_volumes': json_data['total_volumes'][i][1]
+			}, ignore_index=True)
 	
-	# pdb.set_trace()
-	
-	# for coin in json_data:
-	
-	# 	df = df.append({
-	# 		    'id': coin['id'],
-	# 		    'time': scrape_time,
-	#       		'symbol': coin['symbol'],
-	# 			'name': coin['name'],
-	# 			'logo': coin['image'],
-	# 			'current_price': coin['current_price'],
-	# 			'market_cap': coin['market_cap'],
-	# 			'market_cap_rank': coin['market_cap_rank'],
-	# 			'total_volume': coin['total_volume'],
-	# 			'circulating_supply': coin['circulating_supply'],
-	# 			'total_supply': coin['total_supply'],
-	# 			'max_supply': coin['max_supply'],
-	# 			'ath': coin['ath'],
-	# 			'ath_date': coin['ath_date'],
-	# 			'atl': coin['atl'],
-	# 			'atl_date': coin['atl_date'],
-	# 			'roi': coin['roi']
-	# 		}, ignore_index=True)
-	
-# pdb.set_trace()
 
 df.to_csv('parsed_files/historical_coingecko_dataset.csv')
