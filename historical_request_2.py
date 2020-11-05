b9cd161dacde89d8a72088061d8ea734deef0b9c
Carly Bouts <cbouts@g.clemson.edu>
2020-11-04 21:25:50 -0500

    address issue raised while running historical request

--- historical_request_2.py
+++ historical_request_2.py
@@ -6,10 +6,8 @@ import ssl
 import datetime
 import time
 import json
-from bs4 import BeautifulSoup
 import pandas as pd
 import requests
-import pdb
 import csv, sys
 
 
@@ -30,39 +28,44 @@ for row in df_2:
 	geckolink = row.replace("-","%20")
 	# print(row)
 	# print(geckolink)
-
-	print("CMC historical " + name)
-	f = open("historical_html_files/" + name + ".html", "wb")
-	url = "https://coinmarketcap.com/currencies/" + name + "/historical-data/" + "?start=20191101&end=20201101"
-	# pdb.set_trace()
-	try:
-		response = urllib.request.urlopen(url, context=context)
-	except urllib.error.HTTPError as e:
-		print("  HTTPError: {}".format(e.code))
-	except urllib.error.URLError as e:
-		print("  URLError: {}".format(e.reason))
+	if os.path.exists("historical_html_files/" + name + ".html"):
+		print(name + ".html already exists.")
 	else:
-		html = response.read()
-		f.write(html)
-		f.close()
-		print("  success")
+		print("CMC historical " + name)
+		f = open("historical_html_files/" + name + ".html", "wb")
+		url = "https://coinmarketcap.com/currencies/" + name + "/historical-data/" + "?start=20191101&end=20201101"
+		# pdb.set_trace()
+		try:
+			response = urllib.request.urlopen(url, context=context)
+		except urllib.error.HTTPError as e:
+			print("  HTTPError: {}".format(e.code))
+		except urllib.error.URLError as e:
+			print("  URLError: {}".format(e.reason))
+		else:
+			html = response.read()
+			f.write(html)
+			f.close()
+			print("  success")
+		time.sleep(30)
 
-
-	print("GECKO historical " + name)
-	f = open("historical_json_files/" + name + ".json", "w")
-	try:
-		url = 'https://api.coingecko.com/api/v3/coins/' + geckolink + '/market_chart/range?vs_currency=usd&from=1572609600&to=1604235600'
-		# for a test:
-		# 	url = 'http://api.coingecko.com/api/v3/coins/' + 'bitcoin' + '/market_chart/range?vs_currency=usd&from=1572609600&to=1580475600'
-		r = requests.get(url)
-	except requests.HTTPError as e:
-	 	print("  HTTPError: {}".format(e.code))
+	if os.path.exists("historical_json_files/" + name + ".json"):
+		print(name + ".json already exists.")
 	else:
-		data = r.json()
-		f.write(json.dumps(data))
-		f.close()
-		print("  success")
-	time.sleep(60)
+		print("GECKO historical " + name)
+		f = open("historical_json_files/" + name + ".json", "w")
+		try:
+			url = 'https://api.coingecko.com/api/v3/coins/' + geckolink + '/market_chart/range?vs_currency=usd&from=1572609600&to=1604235600'
+			# for a test:
+			# 	url = 'http://api.coingecko.com/api/v3/coins/' + 'bitcoin' + '/market_chart/range?vs_currency=usd&from=1572609600&to=1580475600'
+			r = requests.get(url)
+		except requests.HTTPError as e:
+		 	print("  HTTPError: {}".format(e.code))
+		else:
+			data = r.json()
+			f.write(json.dumps(data))
+			f.close()
+			print("  success")
+		time.sleep(70)
 
 	# print("GECKO historical Q2 " + name)
 	# f = open("historical_json_files/" + name + "Q2.json", "w")
