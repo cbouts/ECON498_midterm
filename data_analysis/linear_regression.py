7e7883392a3d02e64502e34da1d26dffd0540a7e
Carly Bouts <cbouts@g.clemson.edu>
2020-11-07 14:37:18 -0500

    fix linear regression

--- data_analysis/linear_regression.py
+++ data_analysis/linear_regression.py
@@ -2,15 +2,64 @@ from sklearn import linear_model
 import pandas as pd
 
 
+# FOR COINMARKETCAP:
+
 dataset = pd.read_csv("cmc_dataset.csv")
-# print(dataset)
-sorted_dataset = dataset.sort_values(by=['name','time'], ascending=True)
-print(sorted_dataset)
+data = dataset.iloc[:,4].values
+	#  DETERMINING THE CORRELATION BETWEEN MARKET CAP AND PRICE
+target = dataset.iloc[:,6].values
+machine = linear_model.LinearRegression()
+machine.fit(data.reshape(-1, 1), target)
+print("FOR COINMARKETCAP:")
+print("linear regression of prices on market cap:")
+print("intercept: ") 
+print(machine.intercept_)
+print("coefficient: ")
+print(machine.coef_)
 
-target = sorted_dataset.iloc[:,0].values
-# first number is inclusive, second is not. so if we want to get from 3 to 8 we need to do 3:9
-# [columns,rows]
-# this puts it into a long array
+	#  DETERMINING THE CORRELATION BETWEEN MARKET CAP AND VOLUME
+target_2 = dataset.iloc[:,9].values
+machine_2 = linear_model.LinearRegression()
+machine_2.fit(data.reshape(-1, 1), target_2)
+print("linear regression of prices on volume:")
+print("intercept: ")
+print(machine_2.intercept_)
+print("coefficient: ")
+print(machine_2.coef_)
 
-data = sorted_dataset.iloc[:,3:8]
+
+
+# FOR COINGECKO:
+
+dataset_2 = pd.read_csv("coingecko_dataset.csv")
+data_3 = dataset_2.iloc[:,9].values
+	# DETERMINING THE CORRELATION BETWEEN MARKET CAP AND PRICE
+target_3 = dataset_2.iloc[:,6]
+machine_3 = linear_model.LinearRegression()
+machine_3.fit(data_3.reshape(-1, 1), target_3)
+print("   ")
+print("FOR COINGECKO:")
+print("linear regression of prices on volume:")
+print("intercept: ")
+print(machine_3.intercept_)
+print("coefficient: ")
+print(machine_3.coef_)
+	#  DETERMINING THE CORRELATION BETWEEN MARKET CAP AND VOLUME
+target_4 = dataset_2.iloc[:,-1]
+machine_4 = linear_model.LinearRegression()
+machine_4.fit(data_3.reshape(-1, 1), target_4)
+print("linear regression of prices on volume:")
+print("intercept: ")
+print(machine_4.intercept_)
+print("coefficient: ")
+print(machine_4.coef_)
+
+
+
+
+
+
+
+
+
 
